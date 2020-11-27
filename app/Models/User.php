47d05bd4c565c2b6c99c716e314731fb1267e20b
NicolasRGN <regniernicolas37@gmail.com>
2020-11-27 12:10:48 +0100

    Add project

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Adress;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Kyslik\ColumnSortable\Sortable;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Sortable;
+
+    public $sortable = ['firstname','lastname','email', 'city'];
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function addresses() {
+        return $this->hasMany(Adress::class);
+
+    }
 }
